d74ad10f2acf556720c212fcaff03270f382c6e1
AnilkumarAdabala <anilkumara2254@gmail.com>
2023-12-19 20:44:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+let option_1 = document.getElementById("option")
+
+function onclickDiv1(){
+   console.log('hi')
+   let option_1 = document.getElementById("option1")
+    let option_2 = document.getElementById("option2")
+    let option_3 = document.getElementById("option3")
+
+    option_1.style.height = "162px"
+    option_2.style.height = "74px"
+    option_3.style.height = "74px"
+    option_1.style.border = "0.904px solid #007F61"
+    option_2.style.border = '0.904px solid #C8C8C8'
+    option_3.style.border = '0.904px solid #C8C8C8'
+    option_1.style.backgroundColor = "#F4FBF9"
+    option_2.style.backgroundColor = '#FFF'
+    option_3.style.backgroundColor= '#FFF'
+
+
+}
+
+function onclickDiv2(){
+    console.log('hi')
+    let option_1 = document.getElementById("option1")
+    let option_2 = document.getElementById("option2")
+    let option_3 = document.getElementById("option3")
+    option_1.style.height = "74px"
+    option_2.style.height = "162px"
+    option_3.style.height = "74px"
+    option_2.style.border = "0.904px solid #007F61"
+    option_1.style.border = '0.904px solid #C8C8C8'
+    option_3.style.border = '0.904px solid #C8C8C8'
+    option_1.style.backgroundColor = '#FFF'
+    option_2.style.backgroundColor = '#F4FBF9'
+    option_3.style.backgroundColor = '#FFF'
+ 
+ 
+ }
+ function onclickDiv3(){
+    console.log('hi')
+    let option_1 = document.getElementById("option1")
+    let option_2 = document.getElementById("option2")
+    let option_3 = document.getElementById("option3")
+    
+    option_1.style.height = "74px"
+    option_2.style.height = "74px"
+    option_3.style.height = "162px"
+    option_3.style.border = "0.904px solid #007F61"
+    option_1.style.border = '0.904px solid #C8C8C8'
+    option_2.style.border = '0.904px solid #C8C8C8'
+    option_1.style.backgroundColor = '#FFF'
+    option_2.style.backgroundColor = '#FFF'
+    option_3.style.backgroundColor = '#F4FBF9'
+   
+ }
